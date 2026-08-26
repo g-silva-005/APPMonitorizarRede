142a8d249ef688c54dbecba7ccfcb7277e7dbc53
Gustavo <1999goncalo.gs@gmail.com>
2026-08-26 18:53:17 +0100

    "implementacao de um ciclo while para deixar a app mais fluida"

--- main.py
+++ main.py
@@ -2,73 +2,84 @@ import time
 import subprocess
 import  platform
 import funcoes
+while True:
 
-print ()
+        print ()
 
-print("================== M E N U  de  R E D E ==================")
-print("======== 1 - Testar Ligacao a Internet            ========")
-print("======== 2 - Ver IP Local e Nome da Maquina       ========")
-print("======== 3 - Descobrir o IP de um Site (DNS)      ========")
-print("======== 4 - Fazer Ping a um IP Personalizado     ========")
-print("======== 5 - Calculadora de Sub-Rede IPv4         ========")
-print("======== 0 - Sair do Programa                     ========")
-print("==========================================================")
+        print("================== M E N U  de  R E D E ==================")
+        print("======== 1 - Testar Ligacao a Internet            ========")
+        print("======== 2 - Ver IP Local e Nome da Maquina       ========")
+        print("======== 3 - Descobrir o IP de um Site (DNS)      ========")
+        print("======== 4 - Fazer Ping a um IP Personalizado     ========")
+        print("======== 5 - Calculadora de Sub-Rede IPv4         ========")
+        print("======== 0 - Sair do Programa                     ========")
+        print("==========================================================")
 
-print ()
+        print ()
 
-opcao = int(input("Escolhe uma opcao: "))
+        opcao = int(input("Escolhe uma opcao: "))
 
-print ()
+        print ()
 
-if (opcao == 1):
-    print ("A testar ligacao... :)")
-    time.sleep(3)
-    if (funcoes.testa_ligacao()):
-        print ("Sucesso! Tens acesso a internet :)")
-    else:
-        print ("Infelizmente nao tens internet, verifica o teu wifi ou o teu router :(")
+        if (opcao == 1):
+            print ("A testar ligacao... :)")
+            time.sleep(3)
+            if (funcoes.testa_ligacao()):
+                print ("Sucesso! Tens acesso a internet :)")
+            else:
+                print ("Infelizmente nao tens internet, verifica o teu wifi ou o teu router :(")
 
 
-elif (opcao == 2):
-    print ("A descobrir IP Local e Nome da Maquina...")
-    print ()
-    time.sleep(3)
-    funcoes.nomeIP_local()
+        elif (opcao == 2):
+            print ("A descobrir IP Local e Nome da Maquina...")
+            print ()
+            time.sleep(3)
+            funcoes.nomeIP_local()
 
-elif (opcao == 3):
-    print ("Introduz o link do site :)")
-    site = input ("\n : ")
-    print("A verificar o site...")
-    time.sleep(3)
-    print()
-    funcoes.descobrirIPSite(site)
+        elif (opcao == 3):
+            print ("Introduz o link do site :)")
+            site = input ("\n : ")
+            print("A verificar o site...")
+            time.sleep(3)
+            print()
+            funcoes.descobrirIPSite(site)
 
-elif (opcao == 4):
-    ip = input("Introduza o IP que quer fazer o ping: ")
-    print("A enviar Pacotes...")
-    time.sleep(3)
-    print()
-    funcoes.pingIN(ip)
+        elif (opcao == 4):
+            ip = input("Introduza o IP que quer fazer o ping: ")
+            print("A enviar Pacotes...")
+            time.sleep(3)
+            print()
+            funcoes.pingIN(ip)
 
-elif (opcao == 5):
-    rede = input("Introduza o ip que deseja fazer o calculo (com o cidr): ")
-    print ()
-    print ("A calcular... ")
-    time.sleep(3)
-    print ()
-    res=funcoes.calculadora(rede)
+        elif (opcao == 5):
+            rede = input("Introduza o ip que deseja fazer o calculo (com o cidr): ")
+            print ()
+            print ("A calcular... ")
+            time.sleep(3)
+            print ()
+            res=funcoes.calculadora(rede)
 
-    if res:
-        print("=========================================================")
-        print("============       R E S U L T A D O         ============")
-        print(f"========== Rede: {res['rede']}                    ============" )
-        print(f"========== Broadcast: {res['broadcast']}             ============" )
-        print(f"========== Mascara: {res['mascara']}            ============" )
-        print(f"========== Hosts Disponiveis: {res['hosts_uteis']}            ============" )
-        print("=========================================================")
+            if res:
+                print("=========================================================")
+                print("============       R E S U L T A D O         ============")
+                print(f"========== Rede: {res['rede']}                    ============" )
+                print(f"========== Broadcast: {res['broadcast']}             ============" )
+                print(f"========== Mascara: {res['mascara']}            ============" )
+                print(f"========== Hosts Disponiveis: {res['hosts_uteis']}            ============" )
+                print("=========================================================")
 
-    else:
-        print("Inseriu um IP invalido ou sem o CIDR :( (ex: 10.0.0.0/24)")
+            else:
+                print("Inseriu um IP invalido ou sem o CIDR :( (ex: 10.0.0.0/24)")
+
+        elif (opcao == 0):
+            print ("A fechar o Programa...")
+            time.sleep(3)
+            break
+
+        else:
+            print("Introduziu uma opcao invalida, por favor selecione um numero de 1 a 5 ou 0 se pretender sair.")
+            time.sleep(3)
+
 
 
 
